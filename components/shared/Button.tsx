8cc37d1c95499b179e47abb7a379c6af6aa04cfb
Maneth <myethmin01@gmail.com>
2022-01-05 11:13:09 +0530

    Update Button.tsx

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -4,12 +4,11 @@ import styles from '../../styles/Shared.module.scss'
 interface Props {
   children?: React.ReactNode,
   variant?: String,
-  type?: String
 }
 
-const Button = ({children, variant, type}: Props) => {
+const Button = ({children, variant}: Props) => {
   return (
-    <button type={type} className={`${styles.button} ${variant === 'primary' ? styles.btnPrimary : styles.btnSecondary}`}>
+    <button className={`${styles.button} ${variant === 'primary' ? styles.btnPrimary : styles.btnSecondary}`}>
       {children}
     </button>
   )
